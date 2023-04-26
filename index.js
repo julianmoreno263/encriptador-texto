a3cd5b81dbb544fb9fb9f7dc4d6ad4f6094a069b
Julián Moreno <julianmoreno263@gmail.com>
2023-04-26 15:53:59 -0500

    proyecto terminado,creacion de funcion desencriptar y arreglo de estilos css para los botones

--- index.js
+++ index.js
@@ -11,11 +11,39 @@ function encriptar() {
         .replace(/o/gi, "ober")
         .replace(/u/gi, "ufat")
 
-    if (texto.value.length != 0) {
+    if (texto.length != 0) {
         document.getElementById("texto").value = textoCifrado
         tituloMensaje.textContent = "Texto encriptado con éxito"
         parrafo.textContent = "";
         figura.src = "./img/encriptado.jpg"
+    } else {
+        figura.src = "./img/muñeco.png"
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado"
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
+        alert("Debes ingresar algún texto!")
+    }
+
+}
+
+
+function desencriptar() {
+
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje")
+    let parrafo = document.getElementById("parrafo")
+    let figura = document.getElementById("figura")
+    let textoCifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u")
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado
+        tituloMensaje.textContent = "Texto desencriptado con éxito"
+        parrafo.textContent = "";
+        figura.src = "./img/desencriptado.jpg"
     } else {
         figura.src = "./img/muñeco.png"
         tituloMensaje.textContent = "Ningún mensaje fue encontrado"
