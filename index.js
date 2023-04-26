47a6904b18ef0972ce7308e50014c349887d46dc
Julián Moreno <julianmoreno263@gmail.com>
2023-04-26 15:27:35 -0500

    diseño css con responsive design y creación de funcion de encriptar

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function encriptar() {
     let texto = document.getElementById("texto").value;
-    // let tituloMensaje = document.getElementById("titulo-mensaje")
-    // let parrafo = document.getElementById("parrafo")
-    // let figura = document.getElementById("figura")
+    let tituloMensaje = document.getElementById("titulo-mensaje")
+    let parrafo = document.getElementById("parrafo")
+    let figura = document.getElementById("figura")
     let textoCifrado = texto
 
         .replace(/e/gi, "enter")
@@ -11,13 +11,15 @@ function encriptar() {
         .replace(/o/gi, "ober")
         .replace(/u/gi, "ufat")
 
-    if (document.getElementById("texto").value.length != 0) {
+    if (texto.value.length != 0) {
         document.getElementById("texto").value = textoCifrado
-        document.getElementById("titulo-mensaje").textContent = "Texto encriptado con éxito"
-        document.getElementById("parrafo").textContent = "";
-        document.getElementById("figura").src = "./img/encriptado.jpg"
+        tituloMensaje.textContent = "Texto encriptado con éxito"
+        parrafo.textContent = "";
+        figura.src = "./img/encriptado.jpg"
     } else {
-        document.getElementById("figura").src = "./img/muñeco.png"
+        figura.src = "./img/muñeco.png"
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado"
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
         alert("Debes ingresar algún texto!")
     }
 
